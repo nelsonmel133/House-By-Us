8b3997d62bb8ec497cad7ecf71c67eb8a733d33b
Nelson Mel <nelsonmel133@gmail.com>
2026-06-22 15:41:19 +0200

    Update index.ts

--- backend/drizzle/index.ts
+++ backend/drizzle/index.ts
@@ -1,1 +1,18 @@
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: "./schema/*.ts",
+  out: "./migrations",
+  dialect: "mysql",
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT ?? 3306),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+  },
+  verbose: true,
+  strict: true,
+});
 
